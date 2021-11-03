1de212617fb5b85dd7427d8a88a0b80a3352469d
Annad-Ison <90814625+Annad-Ison@users.noreply.github.com>
2021-11-03 03:11:54 +0000

    introducted react-native-navigation

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View, } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./screens/login";
+import Home from "./screens/home";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+    const   MainNavigator = createStackNavigator();
+    return(
+        <View style={{flex: 1}}>
+            <NavigationContainer>
+                <MainNavigator.Navigator screenOptions={{headerShown: false}} initialRouteName="login" >
+                    <MainNavigator.Screen name="home" component={Home} />
+                    <MainNavigator.Screen name="login" component={Login} />
+                </MainNavigator.Navigator>
+            </NavigationContainer>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
