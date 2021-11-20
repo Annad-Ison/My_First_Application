d2f4bf0e45b1004ec064c5ba9ddff9c1f84b5697
Annad-Ison <anaddison@st.ug.edu.gh>
2021-11-20 17:19:27 +0000

    FINAL PROJECT
    5 commit

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Button, View, } from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Login from "./screens/login";
-import Home from "./screens/home";
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MyTabs from './screens/components/navigation';
 
-
-export default function App(){
-    const   MainNavigator = createStackNavigator();
-    return(
-        <View style={{flex: 1}}>
-            <NavigationContainer>
-                <MainNavigator.Navigator screenOptions={{headerShown: false}} initialRouteName="login" >
-                    <MainNavigator.Screen name="home" component={Home} />
-                    <MainNavigator.Screen name="login" component={Login} />
-                </MainNavigator.Navigator>
-            </NavigationContainer>
-        </View>
-    );
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
